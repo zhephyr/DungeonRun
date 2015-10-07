fe6543f6fc0321dc7f1d3d2907d90c4346af89e8
zhephyr <wang.michael619@gmail.com>
2015-10-07 12:24:16 -0500

    Start
    Added files

---
+++ Assets/Scripts/FloorGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class FloorGenerator : MonoBehaviour
+    {
+        void Start()
+        {
+            Mesh mesh = GetComponent<MeshFilter>().mesh;
+            Vector3[] vertices = mesh.vertices;
+            Vector2[] uvs = new Vector2[vertices.Length];
+
+            for (int i = 0; i < uvs.Length; i++)
+            {
+                uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+            }
+            mesh.uv = uvs;
+        }
+    }
+}
